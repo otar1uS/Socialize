3b2cf8ffac98ea562600f0e1b8966391a96c80df
Otari Pkhovelishvili <opkhovelishvili@gmail.com>
2024-01-21 09:28:07 +0400

    test

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/app/api/webhooks/route.ts"],
+  publicRoutes: ["/", "/api/webhooks"],
 });
 
 export const config = {
